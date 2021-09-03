d1cbd6de4815b0d76b1cb2ca78f0bda53d1ae34b
Marcos Huck <marcoshuck@ekumenlabs.com>
2021-09-03 12:47:54 -0300

    Fix test cases

--- main_test.py
+++ main_test.py
@@ -1,14 +1,55 @@
 import os
 import unittest
 
-from main import run
+from yamlpath import YAMLPath
+from main import run, load_file, setup_logger, setup_editor, setup_processor
 
 
 class GenerateConfigYAML(unittest.TestCase):
-    def test_config_file_generated(self):
+    def setUp(self) -> None:
         os.chdir("./examples/local")
+        self.logger = setup_logger()
+        self.editor = setup_editor()
+
+    def test_config_file_generated(self):
         run()
-        self.assertTrue(os.path.isfile('config.yaml'), "config.yaml must exist")
+        self.assertTrue(os.path.exists("config.yaml"))
+
+    def test_version_changed_second_patch(self):
+        run()
+        content = load_file(self.editor, self.logger, "config.yaml")
+        config = setup_processor(self.logger, content)
+        nodes = config.get_nodes(YAMLPath("version"))
+        v = next(nodes, "")
+
+        # It should not be empty
+        self.assertNotEqual(v, "")
+
+        # It should not be 1.0 neither 1.1
+        self.assertNotEqual(v.node, "1.0")
+        self.assertNotEqual(v.node, "1.1")
+
+        # It should be 1.2
+        self.assertEqual(v.node, "1.2")
+
+    def test_added_field_exists(self):
+        run()
+
+        content = load_file(self.editor, self.logger, "config.yaml")
+        config = setup_processor(self.logger, content)
+        nodes = config.get_nodes(YAMLPath("spec.added"))
+        v = next(nodes, "")
+
+        # It should not be empty
+        self.assertNotEqual(v, "")
+
+        # It should contain the patch value
+        self.assertEqual(v.node, "patch")
+
+    def tearDown(self) -> None:
+        if os.path.exists("config.yaml"):
+            os.remove("config.yaml")
+        os.chdir("../..")
 
 
 if __name__ == '__main__':
